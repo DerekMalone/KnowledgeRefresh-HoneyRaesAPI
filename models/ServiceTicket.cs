398e9f636b9e248315e8bc6ec0723385e76d8b10
Derek Malone <laxmalone@gmail.com>
2025-03-22 15:56:10 -1000

    completed chpt 6

--- models/ServiceTicket.cs
+++ models/ServiceTicket.cs
@@ -3,12 +3,12 @@ namespace HoneyRaesAPI.Models;
 public class ServiceTicket
 {
     // Id, CustomerId, EmployeeId, Description, Emergency (this is a boolean), and DateCompleted properties.
-    public int Id;
-    public int? CustomerId;
-    public int? EmployeeId;
-    public string Description;
-    public bool Emergency;
-    public DateTime DateCompleted;
+    public int Id {get; set;}
+    public int? CustomerId {get; set;}
+    public int? EmployeeId {get; set;}
+    public string Description {get; set;}
+    public bool Emergency {get; set;}
+    public DateTime DateCompleted {get; set;}
     public Employee Employee {get; set;}
     public Customer Customer {get; set;}
 }
